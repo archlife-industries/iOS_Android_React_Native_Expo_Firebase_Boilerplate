de3f2473da09d1e8d4b40c4858a8b8f42c32c72d
Ryan Zernach <Ryan@Zernach.com>
2021-09-29 21:18:31 -0400

    adds root_screens

--- src/core/config.tsx
+++ src/core/config.tsx
@@ -14,15 +14,3 @@ import 'firebase/firestore';
 //   // appId: "" // App store ID number, retireved from iOS App Store
 // };
 
-export const FIREBASE_CONFIG = {
-  apiKey: "AIzaSyD1gmo9DCVFQg4Os51xOWXGuLWots7V7O4",
-  authDomain: "archlife-mvp.firebaseapp.com",
-  databaseURL: "https://archlife-mvp.firebaseio.com/",
-  projectId: "archlife-mvp",
-  storageBucket: "archlife-mvp.appspot.com",
-  //storageBucket: "https://console.firebase.google.com/project/archlife-mvp/storage/archlife-mvp.appspot.com/files",
-  messagingSenderId: "633270824942",
-  //measurementId: "",
-  PLIST_VERSION: "2",
-  // appId: "" // App store ID number, retireved from iOS App Store
-};
